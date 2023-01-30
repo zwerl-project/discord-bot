f2cccaf860a2cbec25818c24926951168a1ab222
DownloadableFox <58178791+DownloadableFox@users.noreply.github.com>
2023-01-30 01:00:16 -0500

    Fixed old/new member issue with Miho Event

--- src/events/miho-furry.event.ts
+++ src/events/miho-furry.event.ts
@@ -1,15 +1,16 @@
 import { Events, GuildMember } from 'discord.js';
 import { Event } from '@utils/events';
 
-const ClientReadyEvent: Event = {
+const MihoFurryEvent: Event = {
 	name: Events.GuildMemberUpdate,
 	once: false,
-	async execute(member: GuildMember) {
+	async execute(_: GuildMember, member: GuildMember) {
 		if (member.id !== '1052691949729099917') return;
 
 		const name = member.displayName;
-		if (!name.includes('furry')) await member.setNickname(name + ' (furry)');
+		if (!name.toLowerCase().includes('furry')) 
+			await member.setNickname(name + ' (furry)');
 	}
 };
 
-export default ClientReadyEvent;
+export default MihoFurryEvent;

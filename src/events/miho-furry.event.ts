74f7ab246048f6983e2e190b4bda5116a6def5bf
DownloadableFox <58178791+DownloadableFox@users.noreply.github.com>
2023-01-30 01:04:53 -0500

    Fixed old/new member issue with Miho Event

--- src/events/miho-furry.event.ts
+++ src/events/miho-furry.event.ts
@@ -4,12 +4,14 @@ import { Event } from '@utils/events';
 const MihoFurryEvent: Event = {
 	name: Events.GuildMemberUpdate,
 	once: false,
-	async execute(_: GuildMember, member: GuildMember) {
+	async execute(member: GuildMember) {
 		if (member.id !== '1052691949729099917') return;
+		const guild = member.guild;
 
-		const name = member.displayName;
+		const user = guild.members.cache.find(user => user.id == member.id) as GuildMember;
+		const name = user.displayName;
 		if (!name.toLowerCase().includes('furry')) 
-			await member.setNickname(name + ' (furry)');
+			await user.setNickname(name + ' (furry)');
 	}
 };
 

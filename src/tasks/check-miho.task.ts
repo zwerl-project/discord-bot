85d6057b9f79e381349dafb2050b937f2ee16a1e
DownloadableFox <58178791+DownloadableFox@users.noreply.github.com>
2023-02-05 12:38:01 -0500

    Removed the debug log in check-miho task

--- src/tasks/check-miho.task.ts
+++ src/tasks/check-miho.task.ts
@@ -1,12 +1,10 @@
 import { Client } from 'discord.js';
 import { Task } from '@utils/tasks';
 import config from '@utils/config';
-import logger from '@utils/logger';
 
 const MihoFurryCheck: Task = {
 	schedule: '0 */3 * * *',
 	execute: async (client: Client) => {
-		logger.info('Checking if Miho is a furry...');
 		const guild = client.guilds.cache.get(config.guildId);
 		if (!guild) return;
 

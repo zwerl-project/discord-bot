b66ad86788ed95b00b043851efbe0516cd450341
DownloadableFox <58178791+DownloadableFox@users.noreply.github.com>
2023-07-09 11:51:25 -0500

    fixed lint

--- src/commands/debug/ping.command.ts
+++ src/commands/debug/ping.command.ts
@@ -1,20 +1,20 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
-import { errorWrapper } from "@middlewares";
-import { Command } from "@interfaces";
+import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { errorWrapper } from '@middlewares';
+import { Command } from '@interfaces';
 
 const pingCommand: Command = {
-  data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with pong!"),
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with pong!'),
 
-  middlewares: [errorWrapper],
+	middlewares: [errorWrapper],
 
-  async execute(interaction: CommandInteraction) {
-    await interaction.reply({
-      ephemeral: true,
-      content: `Pong! Recieved at ${Date.now().toString()}`,
-    });
-  },
+	async execute(interaction: CommandInteraction) {
+		await interaction.reply({
+			ephemeral: true,
+			content: `Pong! Recieved at ${Date.now().toString()}`,
+		});
+	},
 };
 
 export default pingCommand;
